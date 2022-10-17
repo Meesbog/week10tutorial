3d03731830ada85a8329bd93a248835dab35ca8c
Meesbog <112994156+Meesbog@users.noreply.github.com>
2022-10-17 09:03:54 +0700

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,39 @@
+var vg_7 = "economy_grouped_bar_chart.vg.json";
+vegaEmbed("#grouped_bar_chart", vg_7).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "final_map.vg.json";
+vegaEmbed("#map", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "GDP_line_chart.vg.json";
+vegaEmbed("#line_chart", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "heatmap.vg.json";
+vegaEmbed("#heatmap", vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_5 = "streamgraph.vg.json";
+vegaEmbed("#streamgraph", vg_5).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_6 = "treemap.vg.json";
+vegaEmbed("#treemap", vg_6).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_7 = "homework.vg.json";
+vegaEmbed("#homework", vg_7).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_8 = "Unemployment.vg.json";
+vegaEmbed("#circle", vg_8).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
